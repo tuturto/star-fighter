ea5dc8ded77e453abeb218aef8c4477ac09aedd9
Tuukka Turto <tuukka.turto@oktaeder.net>
2015-09-25 06:56:43 +0300

    Simplify code

--- StarFighter/Scoring.fs
+++ StarFighter/Scoring.fs
@@ -14,6 +14,6 @@ let scoreRenderer scores res time =
     |> Option.iter (fun x -> 
                         let score, highScore = x
                         let font = res.fonts.Item "blade-48"
-                        let width = font.MeasureString(highScore.ToString())
+                        let width = font.MeasureString(highScore.ToString()).X
                         res.spriteBatch.DrawString(font, score.ToString(), Vector2(10.0f, 10.0f), Color.White)
-                        res.spriteBatch.DrawString(font, highScore.ToString(), Vector2(1009.0f - width.X, 10.0f) , Color.White))
+                        res.spriteBatch.DrawString(font, highScore.ToString(), Vector2(1009.0f - width, 10.0f) , Color.White))
